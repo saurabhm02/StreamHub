0b4b8799808c6a9b32bff2e6a1e2d5063434a152
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-12-08 02:08:55 +0530

    add sidebar with navigation functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -39,11 +39,13 @@ const Header = () => {
     <>
         <div className="flex justify-between px-5 py-3 sticky w-full shadow-lg items-center">
             <div className="flex ">
+               <button className="rounded-full hover:bg-zinc-100 p-2">
                 <RiMenuUnfoldLine 
-                    className="cursor-pointer"
-                    onClick={() => toggleMenuHandler()}
-                />
-                <div className="flex items-start max-md:hidden logo cursor-pointer">
+                        className="cursor-pointer "
+                        onClick={() => toggleMenuHandler()}
+                    />
+               </button>
+                <div className="flex items-start max-md:hidden logo cursor-pointer p-2">
                     <a href="/">
                         <img
                             src={theme === "light" ? logo : darkLogo}
@@ -87,9 +89,15 @@ const Header = () => {
                     <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
                     <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                 </label>
-                <RiVideoAddLine />
-                <IoMdNotificationsOutline />
-                <FaCircleUser />    
+                <div className="max-sm:hidden rounded-full cursor-pointer hover:bg-zinc-100 p-2">
+                    <RiVideoAddLine  />
+                </div>
+                <div className="max-sm:hidden rounded-full cursor-pointer hover:bg-zinc-100 p-2">
+                    <IoMdNotificationsOutline />
+                </div>
+                <div className="max-sm:hidden rounded-full cursor-pointer hover:bg-zinc-100 p-2">
+                    <FaCircleUser /> 
+                </div>   
             </div>
         </div>
     </>
